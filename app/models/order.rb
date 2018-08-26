225567d8fdd201728e9cd5224741ffb0a18fcd03
Katelynn Weingart <kweingart@Andrews-iMac.local>
2018-08-26 17:53:45 -0500

    expiration date validation in place

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,8 +1,16 @@
 class Order < ApplicationRecord
   belongs_to :showtime
 
+  validates :name, :email, :credit_card_number, :expiration_date, :quantity, presence: true
   # need to validate email and credit card
-  validates :email, confirmation: true
+
+
   # need to check the expiration date is not earlier than today's date
+  validate :expiration_date_cannot_be_in_the_past
+  def expiration_date_cannot_be_in_the_past
+    if expiration_date.present? && expiration_date < Date.today
+      errors.add(:expiration_date, "can't be in the past")
+    end
+  end
 
 end
