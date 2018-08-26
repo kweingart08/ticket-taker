6235f3d19842ae67708526e96d764f074c590b2a
Katelynn Weingart <kweingart@Andrews-iMac.local>
2018-08-26 16:05:10 -0500

    tickets sold updated when order is created

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -20,17 +20,25 @@ class OrdersController < ApplicationController
     # render json: params[:order]
     # render json: params[:order][:name]
     # render json: params[:order][:quantity].to_i
-    # expiration_date = params[:order][:expiration_date(2i)] + params[:order][:expiration_date(2i)]
-    #
     # render plain: expiration_date
+
     @order = Order.new({
       name: params[:order][:name],
       email: params[:order][:email],
       credit_card_number: params[:order][:credit_card_number],
       expiration_date: params[:order][:expiration_date],
       quantity: params[:order][:quantity].to_i,
-      showtime_id: params[:order][:showtime_id].to_i
+      showtime_id: params[:showtime_id].to_i
       })
+
+
+      @showtime = Showtime.find(params[:showtime_id])
+      # need to increase the number sold
+      new_tickets = @showtime.tickets_sold + params[:order][:quantity].to_i
+      @showtime.update({
+        tickets_sold: new_tickets
+      })
+
 
     if @order.save
       redirect_to showtime_orders_path
