a43a8505e3e68bbe4ca837a424dd71d992b1aca7
Katelynn Weingart <kweingart@Andrews-iMac.local>
2018-08-26 14:14:05 -0500

    created movie model and routes

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,53 @@
 class MoviesController < ApplicationController
+
+  def index
+    @movies = Movie.all
+  end
+
+  def show
+    @movie = Movie.find(params[:id])
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    # render plain: params[:movie].inspect
+    @movie = Movie.new(movie_params)
+
+    if @movie.save
+      #this will redirect to the show view of that movie
+      redirect_to @movie
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+
+    if @movie.update(movie_params)
+      #this will redirect to the show view of that movie
+      redirect_to @movie
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    redirect_to screens_path
+  end
+
+  private def movie_params
+    params.require(:movie).permit(:title)
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   root 'screens#index'
 
   resources :screens
-
+  resources :movies
 end
