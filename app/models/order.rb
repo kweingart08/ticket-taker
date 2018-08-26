bd5db1c64bb7afb90ea732d889a36cb097db65e9
Katelynn Weingart <kweingart@Andrews-iMac.local>
2018-08-26 17:44:43 -0500

    changed name of submit button on order form

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,8 @@
 class Order < ApplicationRecord
   belongs_to :showtime
+
+  # need to validate email and credit card
+  validates :email, confirmation: true
+  # need to check the expiration date is not earlier than today's date
+
 end
