fde72b3d4064ee13400bfdc20affbf0f1546fc19
Katelynn Weingart <kweingart@Andrews-iMac.local>
2018-08-26 15:50:36 -0500

    order can be created

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,48 @@
 class OrdersController < ApplicationController
 
   def index
-
+    @orders = Order.all
   end
+
+  def show
+    @order = Order.find(params[:id])
+  end
+
+  def new
+    @showtime = Showtime.find(params[:showtime_id])
+    @order = Order.new
+
+    # render json: params[:showtime_id]
+  end
+
+  def create
+
+    # render json: params[:order]
+    # render json: params[:order][:name]
+    # render json: params[:order][:quantity].to_i
+    # expiration_date = params[:order][:expiration_date(2i)] + params[:order][:expiration_date(2i)]
+    #
+    # render plain: expiration_date
+    @order = Order.new({
+      name: params[:order][:name],
+      email: params[:order][:email],
+      credit_card_number: params[:order][:credit_card_number],
+      expiration_date: params[:order][:expiration_date],
+      quantity: params[:order][:quantity].to_i,
+      showtime_id: params[:order][:showtime_id].to_i
+      })
+
+    if @order.save
+      redirect_to showtime_orders_path
+    else
+      render 'new'
+    end
+  end
+
+
+  private def order_params
+  params.require(:order).permit(:name, :email, :credit_card_number, :expiration_date, :quantity, :showtime_id)
+end
+
 
 end
