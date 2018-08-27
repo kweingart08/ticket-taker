d45f7ab7ac1dcce3fa8e740336fab0d6596d3777
Katelynn Weingart <kweingart@Andrews-iMac.local>
2018-08-27 08:15:29 -0500

    added luhn algorithm to check credit card number

--- app/models/order.rb
+++ app/models/order.rb
@@ -16,6 +16,32 @@ class Order < ApplicationRecord
   validates :email, presence: true, format: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 
 
+  # using the luhn algorithm to detect valid card number
+  validate :valid_card
+  def valid_card
+    number = credit_card_number
+    num_array = number.to_s.split("")
+    reverse = num_array.reverse
+
+    for i in 0...reverse.length
+     if i % 2 != 0
+       num = reverse[i].to_i
+       new_num = num * 2
+       reverse[i] = new_num.to_s
+     end
+    end
+    new_array = reverse.join().split("").reverse
+
+    sum = 0
+    for i in 0...new_array.length
+     sum += new_array[i].to_i
+    end
+
+    if sum % 10 != 0
+      errors.add(:credit_card_number, "Card Number Not Valid")
+    end
+  end
+
   # need to validate credit card
 
 end
