a3f700107abcdf1974f0e00dfa45fd6d0c3fd23c
Katelynn Weingart <kweingart@Andrews-iMac.local>
2018-08-27 12:13:09 -0500

    mailer preview added

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,10 +1,9 @@
 class OrderMailer < ApplicationMailer
-  default from: 'orders@example.com'
 
   def new_order(order)
     @order = order
 
-    mail(to: @order.email, subject: "Order Confirmation")
+    mail to: @order.email, subject: "Order Confirmation for #{@order.showtime.movie.title}"
   end
 
 end
--- test/mailers/previews/order_mailer_preview.rb
+++ test/mailers/previews/order_mailer_preview.rb
@@ -1,4 +1,9 @@
 # Preview all emails at http://localhost:3000/rails/mailers/order_mailer
 class OrderMailerPreview < ActionMailer::Preview
 
+  def new_order
+    order = Order.last
+    OrderMailer.new_order(order)
+  end
+
 end
