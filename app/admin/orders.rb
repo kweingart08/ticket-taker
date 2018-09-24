5e6a9519ca00c7bb5ff11516affe0a47a075fe24
Katelynn Weingart <katelynnweingart@Katelynns-MacBook-Pro-2.local>
2018-09-24 15:08:28 -0500

    removed required tree from application.css

--- app/admin/orders.rb
+++ app/admin/orders.rb
@@ -1,38 +1,43 @@
 ActiveAdmin.register Order do
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-permit_params :name, :email, :credit_card_number, :expiration_date, :quantity, :showtime_id
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  permit_params :name, :email, :credit_card_number, :expiration_date, :quantity, :showtime_id
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:permitted, :attributes]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
 
-index do
-  column :id
-  column :name
-  column :email
-  column :movie do |showtime_id|
-    @movie = Showtime.find(id=showtime_id.id).movie.title
+  index do
+    column :id
+    column :name
+    column :email
+    column :movie do |showtime_id|
+      @movie = Showtime.find(id=showtime_id.id).movie.title
+    end
+    column 'Date & Time' do |showtime_id|
+      @time = Showtime.find(id=showtime_id.id).time
+    end
+    column :quantity
+    column :order_total do |order|
+      number_to_currency order.quantity * order.showtime.price
+    end
+    actions
   end
-  column 'Date & Time' do |showtime_id|
-    @time = Showtime.find(id=showtime_id.id).time
-  end
-  column :quantity
-  column :order_total do |order|
-    number_to_currency order.quantity * order.showtime.price
-  end
-  actions
-end
 
-controller do
-  def create
+  controller do
+    def create
 
+    end
   end
-end
+
+  filter :showtime
+  filter :name
+  filter :email
+  filter :quantity
 
 end
