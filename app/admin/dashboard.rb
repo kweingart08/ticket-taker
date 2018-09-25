a5e73e16fd9a59c42ea6ab3c0c397081b6fee193
Katelynn Weingart <katelynnweingart@Katelynns-MacBook-Pro-2.local>
2018-09-25 14:38:35 -0500

    added tables to dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -13,33 +13,14 @@ ActiveAdmin.register_page "Dashboard" do
           end
         end
 
-        column do
-          panel "Dashboard" do
-            div do
-              render("/admin/dashboard/orders_dashboard")
-            end
-          end
-        end
+
     end #columns
 
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
-
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
+    panel "Dashboard" do
+      div do
+        render("/admin/dashboard/orders_dashboard")
+      end
+    end
+    
   end # content
 end
