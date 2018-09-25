9858f798e4d3dca4872c4b629f45366351f78872
Katelynn Weingart <katelynnweingart@Katelynns-MacBook-Pro-2.local>
2018-09-25 14:01:12 -0500

    adding showtimes for movies to movie show page

--- app/admin/movies.rb
+++ app/admin/movies.rb
@@ -1,22 +1,12 @@
 ActiveAdmin.register Movie do
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-permit_params :title
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
 
+  permit_params :title
 
   index do
     column :id
     column :title
     actions
+
   end
 
   form do |f|
@@ -26,5 +16,16 @@ permit_params :title
     f.actions
   end
 
+# on the show page show all showtimes affiliated with that movie
+  show do
+    attributes_table do
+      row :id
+      row :title
+    end
+
+    panel "Showtimes for #{title}" do
+      render 'movie_list', layout: "application"
+    end
+  end
 
 end
