44d9dfe3e276f30bbc0241c9d003080dd8e0eb79
Katelynn Weingart <katelynnweingart@Katelynns-MacBook-Pro-2.local>
2018-09-25 14:53:09 -0500

    changed order show page to not show credit card info

--- app/admin/orders.rb
+++ app/admin/orders.rb
@@ -1,16 +1,6 @@
 ActiveAdmin.register Order do
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
+
   permit_params :name, :email, :credit_card_number, :expiration_date, :quantity, :showtime_id
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:permitted, :attributes]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
 
   index do
     column :id
@@ -30,6 +20,16 @@ ActiveAdmin.register Order do
       number_to_currency order.quantity * order.showtime.price
     end
     actions
+  end
+
+  show do
+    attributes_table do
+      row :id
+      row :name
+      row :email
+      row :quantity
+      row :showtime
+    end
   end
 
 
