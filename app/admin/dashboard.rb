1fbc6a1a482cdc98a649ab80ef3793e885fd6987
Katelynn Weingart <katelynnweingart@Katelynns-MacBook-Pro-2.local>
2018-09-25 14:32:00 -0500

    added movie and order totals to admin dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -7,6 +7,8 @@ ActiveAdmin.register_page "Dashboard" do
           panel "Recent Orders" do
             table_for Order.limit(10) do
               column("Quantity") { |order| order.quantity}
+              column("Movie") { |order| Showtime.find_by(id: order.showtime_id).movie.title}
+              column("Order Total") { |order| number_to_currency order.quantity * order.showtime.price}
             end
           end
         end
