8d944b68b9e3c7b20e4687a252f9d6528e6c73a6
Katelynn Weingart <katelynn@launchpadlab.com>
2018-10-02 14:25:58 -0500

    added required to be true for all values

--- app/models/order_row_importer.rb
+++ app/models/order_row_importer.rb
@@ -4,9 +4,9 @@ class OrderRowImporter < CSVImportable::RowImporter
       showtime_id: pull_string('showtime_id', required: true),
       email: pull_string('email', required: true),
       credit_card_number: pull_string('credit_card_number', required: true),
-      expiration_date: pull_string('expiration_date'),
-      quantity: pull_float('quantity'),
-      name: pull_string('name')
+      expiration_date: pull_string('expiration_date', required: true),
+      quantity: pull_float('quantity', required: true),
+      name: pull_string('name', required: true)
     )
   end
 end
